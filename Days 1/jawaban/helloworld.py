a23341d1bf8c86b7381bbf71d11f71ee5138bd90
Akasyah Archie <54403225+SpectreOfSolitude@users.noreply.github.com>
2022-12-17 02:24:56 +0700

    Add files via upload

---
+++ Days 1/jawaban/helloworld.py
@@ -0,0 +1,23 @@
+#2
+print (5+6)
+print (5-6)
+print (5*6)
+print (6%5)
+print (5/6)
+print (5**6)
+print (5//6)
+
+#3
+print("Akasyah Muhammad Arsyi")
+print("105221023")
+print("Jakarta Selatan")
+
+#4
+print (type (12))
+print (type (7.4))
+print (type (3.14))
+print (type (5-6j))
+print (type (["Budi", "105201001", "Jakarta"]))
+print (type ("Akasyah Muhammad Arsyi"))
+print (type (105221023))
+print (type ("Jakarta Selatan"))
