e86c7baa017225b4967ea17ca63a8421ca255d04
Akasyah Archie <54403225+SpectreOfSolitude@users.noreply.github.com>
2022-12-23 13:50:14 +0700

    Final Answer

---
+++ Days 1/jawaban/Level3.py
@@ -0,0 +1,31 @@
+#Tuliskan contoh dari tipe data yang berbeda seperti Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set dan Dictionary.
+#Tentukan jarak Euclidian antara (2, 3) dan (10, 8)
+
+#1
+
+#Integer:
+print (10)
+#Float: 
+print (3.14)
+#Complex: 
+print (1+2j)
+#String: 
+print ("Spectre")
+#Boolean: 
+print (True)
+#List: 
+A = [1, 4, 6,]
+#Tuple: 
+T = ("Spectre", 1, "Solitude", 2)
+#Set: 
+S = ([2, 4, 6, 8, 10])
+
+#Dictionary:
+Dct = {1:'Spectre', 2:'Solitude'}
+
+#2
+
+from math import dist
+point1 = (2, 3)
+point2 = (10, 8)
+dist_math = dist (point1, point2)
