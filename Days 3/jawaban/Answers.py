e24016cca679caa130bd7ad77b843ab8ec958410
SpectreOfSolitude <kakak16903@gmail.com>
2022-12-24 01:03:17 +0700

    First Commit - Level 1 done
    Level 1 Task day 3 sudah selesai, tersisa level 2

---
+++ Days 3/jawaban/Answers.py
@@ -0,0 +1,38 @@
+# Day 3
+
+# Lvl 1
+umur = 19
+tinggi = 171.5
+comp = (2j +5)
+
+alas = input("Masukkan alas: ")
+tinggi = input("Masukkan tinggi: ")
+luas = alas * tinggi /2
+print ("==============")
+print("Luas segitiga: "+luas)
+
+a = input("Masukkan sisi A: ")
+b = input("Masukkan sisi B: ")
+c = input("Masukkan sisi C: ")
+Keliling = a+b+c
+print ("==============")
+print("Keliling segitiga: "+Keliling)
+
+panjang = input("Masukkan panjang: ")
+lebar = input("Masukkan lebar: ")
+LuasPersegiPanjang = panjang*lebar
+KelilingPersegiPanjang = 2*(panjang*+lebar)
+print("======================")
+print ("Luas persegi panjang: "+LuasPersegiPanjang)
+print ("Keliling persegi panjang: "+KelilingPersegiPanjang)
+
+r = input("Masukkan jari-jari: ")
+pi = 3.14
+LuasLingkaran = pi*r*r
+KelilingLingkaran=pi*r*2
+print ("==============================")
+print ("Luas LIngkaran: "+LuasLingkaran)
+print ("Keliling Lingkaran: "+KelilingLingkaran)
+
+# Lvl 2
+
