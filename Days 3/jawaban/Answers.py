5137e9ae1055873dbcb4a15ee2c4fde5a6bc9617
SpectreOfSolitude <kakak16903@gmail.com>
2022-12-30 00:38:02 +0700

    Level 2 progress update
    no 1, 2 & 3

--- Days 3/jawaban/Answers.py
+++ Days 3/jawaban/Answers.py
@@ -37,20 +37,39 @@ print ('Luas LIngkaran: %0.2f' %LuasLingkaran)
 print ('Keliling Lingkaran: %0.2f' %KelilingLingkaran)
 
 # Lvl 2
+#1
 print("====================")
 print("Lvl 2")
 print("====================")
 
-x= int(input("input x: "))
-y = (2*(x) -2)
-print("y = (2*(x) -2)")
-print("y = %0.2f" %y)
+print(("y = 2x - 2"))
+y2 = 2
+x2 = 1
+y1 = 0
+x1 = 0
+m = ((y2-y1)/(x2-x1)) 
+print("slope = %0.2f" %m)
+ycut = -2
+print("y intercept: %0.2f" %ycut)
+xcut = 1
+print("x intercept: %0.2f" %xcut)
 
-
-import numpy as np
+#2
+print("====================")
 print ("point 1 = (2, 2)")
 print ("point 2 = (6, 10)")
-point1 = np.array([2,2])
-point2 = np.array([6, 10])
-D = np.linalg.norm(point2-point1)
-print("D = %0.2f" %D)
+x1 = 2
+x2 = 6
+y1 = 2
+y2 = 10
+m = ((y2-y1)/(x2-x1))
+d = ((y2-y1)**2)+((x2-x1)**2)
+print("Slope = %0.2f" %m)
+print ("Euclidian Distance = %0.2f" %d)
+
+#3
+print ("Slope in task 1 == slope in task 2")
+
+#4
+print ("===============================")
+print ("y = x^2 +6x + 9")
